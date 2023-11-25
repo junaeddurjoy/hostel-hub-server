c1dfa8fb3bd5328998362cd06d19af4d5c7ff194
junaeddurjoy <junaed.durjoy@gmail.com>
2023-11-25 21:21:54 +0600

    cors updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173","http://localhost:5174"],
   credentials: true
 }));
 app.use(express.json());
